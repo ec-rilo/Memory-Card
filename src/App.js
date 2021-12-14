7c8de76e6849a1dcd5695c9b7ee9f6376bedfc10
Edgar <ecarrillo046@gmail.com>
2021-12-14 11:28:06 -0800

    feat: import Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import HeaderContent from './components/Header/HeaderContent';
 import './styles/App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HeaderContent></HeaderContent>
+    </div>
+  );
 }
 
 export default App;
