fd292298d972c2cde0b816c3e25697e94072a891
Edgar <ecarrillo046@gmail.com>
2021-12-15 10:50:40 -0800

    feat: create CardsContainer

--- src/components/Body/CardsContainer.js
+++ src/components/Body/CardsContainer.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Card from './Card';
+import movieData from './Logic/TMDb';
 
 const CardsContainer = () => {
-  return <section></section>;
+  const [cards, setCards] = useState([]);
+
+  const addCards = async () => {
+    const tempArr = [];
+    for (let i = 0; i < 12; ++i) {
+      const obj = await movieData.getPosterData(i);
+      tempArr.push(obj);
+    }
+    setCards(tempArr);
+  };
+  useEffect(() => {
+    if (cards.length < 12) {
+      addCards();
+    }
+  });
+
+  return (
+    <section className="cards-container">
+      {cards.map((card) => {
+        return <Card key={card.key} src={card.src} alt={card.alt} />;
+      })}
+    </section>
+  );
 };
 
 export default CardsContainer;
