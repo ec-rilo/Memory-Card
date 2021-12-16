e3ab6c21759289128e37ab89c6c89cddcf501f7d
Edgar <ecarrillo046@gmail.com>
2021-12-15 21:00:09 -0800

    feat(test): test receiveHit() and isHit() functions

--- src/Logic/__tests__/gameboard.test.js
+++ src/Logic/__tests__/gameboard.test.js
@@ -1,4 +1,5 @@
 import { gbAllSelected, gbOneNotSelected } from '../mocks/allSelected';
+import { gbreceiveHit, gbreceiveHit2 } from '../mocks/receiveHit';
 
 describe('gameboard() tests', () => {
   test('function allSelected() returns true if all items in cardsArr are selected', () => {
@@ -7,5 +8,23 @@ describe('gameboard() tests', () => {
 
   test("function allSelected() returns false if one item's `selected` prop is false in cardsArr", () => {
     expect(gbOneNotSelected.allSelected()).toBeFalsy();
+  });
+
+  test('function receiveHit() can mark an obj in the cardsArr as hit: true', () => {
+    expect(gbreceiveHit.getCardsArr()[5].selected).toBeTruthy();
+    for (let i = 0; i < 5; ++i) {
+      expect(gbreceiveHit.getCardsArr()[i].selected).toBeFalsy();
+    }
+    for (let i = 6; i < 12; ++i) {
+      expect(gbreceiveHit.getCardsArr()[i].selected).toBeFalsy();
+    }
+  });
+
+  test('function isDoubleHit() returns false when a card calls receiveHit() once', () => {
+    expect(gbreceiveHit.isDoubleHit()).toBeFalsy();
+  });
+
+  test('function isDoubleHit() returns true when a card calls receiveHit() twice', () => {
+    expect(gbreceiveHit2.isDoubleHit()).toBeTruthy();
   });
 });
