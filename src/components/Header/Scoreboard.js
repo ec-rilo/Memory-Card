4209255284f1e4a8e8fad4e269e92f1618fb5deb
Edgar <ecarrillo046@gmail.com>
2021-12-16 15:06:08 -0800

    nest component in a parent div

--- src/components/Header/Scoreboard.js
+++ src/components/Header/Scoreboard.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const Scoreboard = (props) => {
   return (
-    <div className="score-container">
-      <p>
-        SCORE: <span className="score">0</span>
-      </p>
-      <p>
-        BEST: <span className="best-score">0</span>
-      </p>
+    <div className="score-content">
+      <div className="score-container">
+        <p>
+          SCORE: <span className="score">0</span>
+        </p>
+        <p>
+          BEST: <span className="best-score">0</span>
+        </p>
+      </div>
     </div>
   );
 };
