8820be8f434d207a0c1c9a4e516d530d150e81ac
Edgar <ecarrillo046@gmail.com>
2021-12-16 10:54:49 -0800

    feat: add startGame() function

--- src/Logic/game.js
+++ src/Logic/game.js
@@ -20,7 +20,37 @@ const game = (() => {
     playing = false;
   };
 
-  return { isPlaying, setCardsArr, reset };
+  const startGame = (arr) => {
+    const screenScore = document.querySelector('.score');
+    const screenBestScore = document.querySelector('.best-score');
+
+    playing = true;
+    cardsArr = arr;
+    gameboard.updateCardsArr(arr);
+
+    cardsArr.forEach((card) => {
+      const tempCard = document.getElementById(`${card.uniqid}`);
+      tempCard.addEventListener('click', () => {
+        gameboard.receiveHit(card.uniqid);
+        if (gameboard.allSelected()) {
+          console.log('you win!');
+        } else if (!gameboard.isDoubleHit()) {
+          scoreboard.incrementScore();
+          screenScore.innerText = scoreboard.getScore();
+        } else {
+          gameboard.resetHits();
+          if (scoreboard.getBestScore() < scoreboard.getScore()) {
+            scoreboard.setBestScore(scoreboard.getScore());
+            screenBestScore.innerText = scoreboard.getBestScore();
+          }
+          scoreboard.resetCurrScore();
+          screenScore.innerText = scoreboard.getScore();
+        }
+      });
+    });
+  };
+
+  return { isPlaying, setCardsArr, reset, startGame };
 })();
 
 export default game;
