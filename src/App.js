afdd1e15d4322f99039bd27ffc7397f9abce95d4
Edgar <ecarrillo046@gmail.com>
2021-12-16 11:53:58 -0800

    feat: add WinPopup component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import CardsContainer from './components/Body/CardsContainer';
+import WinPopup from './components/Body/WinPopup';
 import Footer from './components/Footer/Footer';
 import HeaderContent from './components/Header/HeaderContent';
 import './styles/App.css';
@@ -6,6 +7,7 @@ import './styles/App.css';
 function App() {
   return (
     <div className="App">
+      <WinPopup />
       <HeaderContent></HeaderContent>
       <CardsContainer />
       <Footer />
