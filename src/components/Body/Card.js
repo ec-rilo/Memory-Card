d71c26e23e47fc3d2678d224e8d795d8296f7e72
Edgar <ecarrillo046@gmail.com>
2021-12-16 11:58:44 -0800

    feat: add props destructuring and update props on jsx elements

--- src/components/Body/Card.js
+++ src/components/Body/Card.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
 const Card = (props) => {
+  const { src, alt, shuffleCards, uniqid } = props;
   // style the card
   // populate an image within it from a prop passed down to it
   return (
     <div className="card">
-      <img
-        src={props.src}
-        alt={props.alt}
-        onClick={() => props.shuffleCards()}
-      ></img>
+      <img src={src} alt={alt} onClick={() => shuffleCards()} id={uniqid}></img>
     </div>
   );
 };
