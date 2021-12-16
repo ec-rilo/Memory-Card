b1d3a30f896c8e9aa4237ca1299fa9e59adb41b2
Edgar <ecarrillo046@gmail.com>
2021-12-16 11:59:03 -0800

    feat: remove commments

--- src/components/Body/Card.js
+++ src/components/Body/Card.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 const Card = (props) => {
   const { src, alt, shuffleCards, uniqid } = props;
-  // style the card
-  // populate an image within it from a prop passed down to it
   return (
     <div className="card">
       <img src={src} alt={alt} onClick={() => shuffleCards()} id={uniqid}></img>
